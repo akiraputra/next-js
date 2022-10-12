174882b0872c787e20fbdf4b15856cc52930ee9d
Putra Syahwal Alam <50631694+akiraputra@users.noreply.github.com>
2022-10-12 14:24:28 +0700

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,10 @@
+export default function blog() {
+    return (
+      <>
+        <h1>Niagahoster Blog</h1>
+        <p>
+          Niagahoster Blog menyajikan berbagai artikel tutorial, digital marketing, web development, dan insight bisnis.
+        </p>
+      </>
+    );
+  }
